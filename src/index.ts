2a9a7705855feaa51fa850be546d098b80358d5d
Igor Davydychev <igor.davydychev@gmail.com>
2019-12-08 12:43:33 +0200

    Implement passing Tunnistamo API tokens downstream
    This gateway has the following flow for authentication:
    - when a user logs in with Tunnistamo, they get an id_token and
      and access token
    - with the access token, they can ask Tunnistamo for API tokens
      for the API scopes, the frontend client is allowed to access
    - frontend needs to pass the entire json with those API tokens
      to the gateway in the "Api-Tokens" header
    - the gateway server parses that header and uses appropriate
      token, when calling each downstream API
    
    This way, we can keep the current Tunnistamo auth flow intact,
    where one API token can only be used for one API. We also
    currently do not want to implement user authentication directly
    at the gateway level, as the apollo-federation docs suggest,
    since 1) we don't have readymade solutions for this in TS/JS,
    2) we haven't fully investigated the security implications of
    this approach (what a breach at the gateway would entail).
    Maybe in the future, if these two issues get required attention,
    gateway will handle the auth, but not now.

--- src/index.ts
+++ src/index.ts
@@ -11,24 +11,45 @@ const port: number = parseInt(process.env.PORT);
 const openCityProfileBackend: string = process.env.OPEN_CITY_PROFILE_API_URL;
 const berthReservationsBackend: string = process.env.BERTH_RESERVATIONS_API_URL;
 
+
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+    // Make the class accept "name" property
+    constructor(
+        config?: Partial<AuthenticatedDataSource> &
+            object &
+            ThisType<AuthenticatedDataSource>,
+    ) {
+        super();
+        if (config) {
+            return Object.assign(this, config);
+        }
+    }
+    name!: string;
+
+    willSendRequest({ request, context }) {
+        // (context as any) due to typescript-related bug in apollo-gateway:
+        // https://github.com/apollographql/apollo-server/issues/3307
+        if ((context as any).apiTokens) {
+            let apiTokens = JSON.parse((context as any).apiTokens);
+
+            request.http.headers.set(
+                "Authorization",
+                "Bearer " + apiTokens[this.name]
+            );
+        }
+    }
+}
+
+
 const gateway = new ApolloGateway({
     serviceList: [
-        { name: 'open-city-profile', url: openCityProfileBackend },
-        { name: 'berth-reservations', url: berthReservationsBackend },
+        // name of the service is the same as its API scope for auth purposes
+        { name: "https://api.hel.fi/auth/profiles", url: openCityProfileBackend },
+        { name: "https://api.hel.fi/auth/berths", url: berthReservationsBackend },
     ],
-    buildService({ url }) {
-        return new RemoteGraphQLDataSource({
-            url,
-            willSendRequest({ request, context }) {
-                request.http.headers.set(
-                    "Authorization",
-                    // typescript-related bug in apollo-gateway:
-                    // https://github.com/apollographql/apollo-server/issues/3307
-                    (context as any).token
-                );
-            }
-        });
-    }
+    buildService({ name, url }) {
+        return new AuthenticatedDataSource({ name, url });
+    },
 });
 
 
@@ -37,8 +58,8 @@ const gateway = new ApolloGateway({
         gateway,
         subscriptions: false,
         context: ({ req }) => {
-            const token = req.headers.authorization || "";
-            return { token };
+            const apiTokens = req.headers["api-tokens"] || "";
+            return { apiTokens };
         },
         debug: debug,
         playground: debug,
