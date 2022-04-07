f799b4d797caaa5f5f4b2421c146e10a07ed73ac
Niko Lindroos <niko.j.makela@gmail.com>
2022-04-07 15:55:07 +0300

    Revert "Add API connection check to the health check"

--- src/index.ts
+++ src/index.ts
@@ -1,101 +1,73 @@
 import { ApolloServer } from "apollo-server-express";
-import { ApolloGateway } from "@apollo/gateway";
+import { ApolloGateway } from '@apollo/gateway';
 import * as cors from "cors";
-import * as dotenv from "dotenv";
+import * as dotenv  from "dotenv";
 import * as express from "express";
-import { AuthenticatedDataSource } from "./dataSources";
-import {
-  berthReservationsBackend,
-  openCityProfileBackend,
-  testConnectionToBerthReservationsBackend,
-  testConnectionToOpenCityProfileBackend,
-} from "./services";
+import { AuthenticatedDataSource } from "./dataSources"
 
 dotenv.config();
 
-const debug: boolean =
-  process.env.DEBUG === "debug" || process.env.NODE_ENV !== "production";
+const debug: boolean = process.env.DEBUG === "debug" || process.env.NODE_ENV !== "production";
 
 const port: string = process.env.PORT || "3000";
+const openCityProfileBackend: string = process.env.OPEN_CITY_PROFILE_API_URL;
+const berthReservationsBackend: string = process.env.BERTH_RESERVATIONS_API_URL;
 
 const gateway = new ApolloGateway({
-  serviceList: [
-    // name of the service is the same as its API scope for auth purposes
-    {
-      name: "https://api.hel.fi/auth/helsinkiprofile",
-      url: openCityProfileBackend,
+    serviceList: [
+        // name of the service is the same as its API scope for auth purposes
+        { name: "https://api.hel.fi/auth/helsinkiprofile", url: openCityProfileBackend },
+        { name: "https://api.hel.fi/auth/berths", url: berthReservationsBackend },
+    ],
+    buildService({ name, url }) {
+        return new AuthenticatedDataSource({ name, url });
     },
-    { name: "https://api.hel.fi/auth/berths", url: berthReservationsBackend },
-  ],
-  buildService({ name, url }) {
-    return new AuthenticatedDataSource({ name, url });
-  },
-  // "Polling running services is dangerous and not recommended in production.
-  // Polling should only be used against a registry.
-  // If you are polling running services, use with caution"
-  // - https://github.com/apollographql/apollo-server/discussions/4280.
-  // experimental_pollInterval: 600000, // every 10 min
+    experimental_pollInterval: 600000,  // every 10 min
 });
 
+
 (async () => {
-  const server = new ApolloServer({
-    gateway,
-    subscriptions: false,
-    context: ({ req }) => {
-      const apiTokens: string = req.headers["api-tokens"] || "";
-      const acceptLanguage: string = req.headers["accept-language"] || "";
-      return { apiTokens, acceptLanguage };
-    },
-    debug: debug,
-    playground: debug,
-    introspection: debug,
-  });
-
-  const app = express();
-
-  app.use(cors());
-
-  // GraphQL Voyager schema visualization
-  if (debug) {
-    const voyagerMiddleware = require("graphql-voyager/middleware").express;
-    app.use(
-      "/voyager",
-      voyagerMiddleware({
-        endpointUrl: "/graphql",
-        displayOptions: {
-          sortByAlphabet: true,
+    const server = new ApolloServer({
+        gateway,
+        subscriptions: false,
+        context: ({ req }) => {
+            const apiTokens: string = req.headers["api-tokens"] || "";
+            const acceptLanguage: string = req.headers["accept-language"] || "";
+            return { apiTokens, acceptLanguage };
         },
-      })
+        debug: debug,
+        playground: debug,
+        introspection: debug
+    });
+
+    const app = express();
+
+    app.use(cors());
+
+    // GraphQL Voyager schema visualization
+    if (debug) {
+        const voyagerMiddleware = require('graphql-voyager/middleware').express;
+        app.use('/voyager', voyagerMiddleware({
+            endpointUrl: '/graphql',
+            displayOptions: {
+                sortByAlphabet: true,
+            },
+        }));
+    }
+
+    // TODO: check that app actually works
+    app.get("/readiness", ( req, res ) => {
+        res.status(200).json({status: "OK"});
+    });
+
+    app.get( "/healthz", ( req, res ) => {
+        res.status(200).json({status: "OK"});
+    });
+
+    server.applyMiddleware({ app, path: "/" });
+
+    app.listen({ port }, () =>
+        // eslint-disable-next-line no-console
+        console.log(`🚀 Server ready at http://localhost:${port}`)
     );
-  }
-
-  // TODO: check that app actually works
-  app.get("/readiness", (req, res) => {
-    res.status(200).json({ status: "OK" });
-  });
-
-  app.get("/healthz", async (req, res) => {
-    const isBerthApiHealthy = await testConnectionToBerthReservationsBackend();
-    const isProfileApiHealthy = await testConnectionToOpenCityProfileBackend();
-    let messages: string[] = [];
-    if (!isBerthApiHealthy) {
-      messages.push("Connection issues with the Berth API.");
-    }
-    if (!isProfileApiHealthy) {
-      messages.push("Connection issues with the Open City Profile API.");
-    }
-    // 504 Gateway Timeout
-    if (messages.length > 0) {
-      res.status(504).json({ status: "ERROR", messages });
-    } else {
-      res.status(200).json({ status: "OK" });
-    }
-  });
-
-  server.applyMiddleware({ app, path: "/" });
-
-  app.listen({ port }, () =>
-    // eslint-disable-next-line no-console
-    console.log(`🚀 Server ready at http://localhost:${port}`)
-  );
 })();
